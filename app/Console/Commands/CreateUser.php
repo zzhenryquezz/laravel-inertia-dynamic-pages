f26a5469e4946247252cc65ce3647644805f81f4
Henrique <henrique@sidekick-coder.com>
2025-06-21 12:51:58 -0300

    feat: add CreateUser command to facilitate user creation

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {--name=} {--email=} {--password=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->option('name') ?? $this->ask('Name');
+        $email = $this->option('email') ?? $this->ask('Email');
+        $password = $this->option('password') ?? $this->secret('Password');
+
+        if (User::where('email', $email)->exists()) {
+            $this->error('A user with this email already exists.');
+            return 1;
+        }
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info('User created successfully: ' . $user->email);
+        return 0;
+    }
+}
